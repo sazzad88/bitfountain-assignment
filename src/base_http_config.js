69c8a9e477ff4a1f6bf196e2dfa305c6d8eea665
sazzadur rahman <sazzadurrahman88@gmail.com>
2021-06-07 11:37:11 +0600

    global error interceptor added

--- src/base_http_config.js
+++ src/base_http_config.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 import { loadProgressBar } from "axios-progress-bar";
 import app_data from "./app_config.json";
+import { tryLogout } from "./store/actions";
 import app_redux_store from "./store/store";
+
+const errorHandler = (error) => {
+  //console.log({ error });
+  //console.log("global error interceptor : ", error);
+  if (error.response) {
+    if (error.response.status === 403) {
+      app_redux_store.dispatch(tryLogout());
+    }
+  }
+
+  return Promise.reject({ ...error });
+};
 
 // Default config options
 const defaultOptions = {
@@ -22,5 +35,10 @@ instance.interceptors.request.use(function (config) {
 });
 
 loadProgressBar(null, instance);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => errorHandler(error)
+);
 
 export default instance;
--- src/components/DeviceContainer.tsx
+++ src/components/DeviceContainer.tsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import Device from "./Device";
 import Overview from "./Overview";
 import ProgressIndicator from "./Utility/ProgressIndicator";
+import TextInputElement from "./Utility/TextInputElement";
 import { fetchDeviceTypes, setNetworkRequest } from "../store/actions";
 import { ModelType, DeviceTypeMap, Store, OverviewType } from "../store/types";
 import { AxiosResponse } from "axios";
@@ -12,6 +13,7 @@ function DeviceContainer() {
   const dispatch = useDispatch();
   const typesMap: DeviceTypeMap = useSelector((state: Store) => state.typesMap);
   const [fetchingDevices, setfetchingDevices] = useState<Boolean>(false);
+  const [keyFilter, setFilterkey] = useState<string>("");
   const [models, setModels] = useState<ModelType[] | []>([]);
   const [overview, setOverview] = useState<OverviewType[] | []>([]);
   const [showOverviewModal, setShowModal] = useState<Boolean>(false);
@@ -31,10 +33,12 @@ function DeviceContainer() {
     axios
       .get(`overview/modeldata/${device.BrandId}/${device.Name}`)
       .then((response: AxiosResponse) => {
-        //console.log(response.data);
         dispatch(setNetworkRequest(false));
         let data: Array<OverviewType> = response.data;
         setOverview(data);
+      })
+      .catch((e) => {
+        console.log(e);
       });
   };
 
@@ -42,11 +46,16 @@ function DeviceContainer() {
     dispatch(fetchDeviceTypes(axios));
   };
   const fetchDevices = () => {
-    axios.get("overview/modeltype").then((response: AxiosResponse) => {
-      let data: Array<ModelType> = response.data;
-      setfetchingDevices(false);
-      setModels(data);
-    });
+    axios
+      .get("overview/modeltype")
+      .then((response: AxiosResponse) => {
+        let data: Array<ModelType> = response.data;
+        setfetchingDevices(false);
+        setModels(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   useEffect(() => {
@@ -57,17 +66,47 @@ function DeviceContainer() {
 
   return (
     <div className="main-container">
-      <div className="section-title full-width">Available Models</div>
+      <div className="section-title full-width">
+        <div className="title">
+          <div>Available Models</div>
+          <div className="filter-container">
+            <TextInputElement
+              value={keyFilter}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setFilterkey(event.target.value);
+              }}
+              placeholder={"Search name,brand..."}
+            />
+          </div>
+        </div>
+        <div className="add-button-container">
+          <button className="btn">Add Device Model</button>
+        </div>
+      </div>
 
       <div className="devices-container full-width">
-        {models.map((model: ModelType) => (
-          <Device
-            model={model}
-            fetchModelOverview={fetchModelOverview}
-            typesMap={typesMap}
-            key={`${model.Id}`}
-          />
-        ))}
+        {models
+          .filter((model: ModelType) => {
+            if (keyFilter.trim().length > 0) {
+              return (
+                model.BrandId?.toLowerCase().indexOf(
+                  keyFilter.toLowerCase().trim()
+                ) !== -1 ||
+                model.Name?.toLowerCase().indexOf(
+                  keyFilter.toLowerCase().trim()
+                ) !== -1
+              );
+            }
+            return true;
+          })
+          .map((model: ModelType) => (
+            <Device
+              model={model}
+              fetchModelOverview={fetchModelOverview}
+              typesMap={typesMap}
+              key={`${model.Id}`}
+            />
+          ))}
         {fetchingDevices ? (
           <div style={{ minHeight: "500px", width: "100%" }}>
             <ProgressIndicator message={"Fetching Devices....."} />
